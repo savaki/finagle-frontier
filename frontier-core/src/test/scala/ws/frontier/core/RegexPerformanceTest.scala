0c68d0f53886223760144def567a33c9ce7634cb
matt <jagdp2r!>
2013-05-01 09:09:38 -0700

    - added tools directory to project so we don't need to fiddle around with different sbt versions - added an sbt to the home directory

---
+++ frontier-core/src/test/scala/ws/frontier/core/RegexPerformanceTest.scala
@@ -0,0 +1,11 @@
+package ws.frontier.core
+
+import ws.frontier.test.TestSuite
+
+/**
+ * @author matt
+ */
+
+class RegexPerformanceTest extends TestSuite{
+
+}
---
+++ sbt
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+`dirname $0`/tools/sbt-0.12.3/bin/sbt $*
